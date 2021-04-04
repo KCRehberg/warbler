5e1ee50f2cd97c56dd0b81e2f2d728e88439f4d4
KCRehberg <krehberg12@gmail.com>
2021-04-04 19:34:50 -0400

    Twitter clone project

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# warbler
+# Warbler Solution
+
+## Getting started
+
+1.  In the `warbler-server` folder
+
+    * `npm install`
+    * `nodemon` (if installed) or `node index.js`
+
+2.  In the `warbler-client` folder
+
+    * `npm install`
+    * `npm start`
--- warbler-server/models/index.js
+++ warbler-server/models/index.js
@@ -1,13 +1,9 @@
-const mongoose = require('mongoose');
-
-mongoose.set('debug', true);
-mongoose.set('useUnifiedTopology', true);
+const mongoose = require("mongoose");
+mongoose.set("debug", true);
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://127.0.0.1:27017/warbler", { 
-    keepAlive: true,
-    useCreateIndex: true,
-    useNewUrlParser: true
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/warbler", {
+  keepAlive: true
 });
 
-module.exports.User = require('./user');
-module.exports.Message = require('./message');
+module.exports.User = require("./user");
+module.exports.Message = require("./message");
